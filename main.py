d40e122843e3d25772ff38d67b8c44a05f555198
ash408 <124532093+ash408@users.noreply.github.com>
2025-08-22 07:23:14 -0500

    Added letter dict for rot

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import string
+
+#Setup argparse (I'm lazy)
+
+def main(working_dir, cipher):
+	text_path = working_dir + "/plain.txt"
+	file_exists = os.path.exists(text_path)
+
+	if (file_exists):
+		generate(cipher, text_path)
+	else:
+		print("No plaintext file found at: " + text_path)
+		
+
+def generate(cipher, text_path):		
+	#add multiple types of ciphers, just hardcoded for now
+	if(cipher != "rot13"):
+		print("Unsupported cipher type")
+		sys.exit()
+
+	#move this out of this file, once again just being lazy
+	list_letters = list(string.ascii_lowercase)
+	letter_dict = {}
+	index = 0
+	
+	for letter in list_letters:
+		letter_dict[letter] = index
+		index = index + 1
+	print(letter_dict)
+		
+
+if __name__ == "__main__":
+	working_dir = os.path.dirname(os.path.realpath(__file__))
+	main(working_dir, "rot13")
